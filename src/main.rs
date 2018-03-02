32088e47d6489f102642ba5cb986f2ccbfb41964
Hani Awni <hani.awni@gmail.com>
2018-03-02 15:35:34 -0500

    add serde & serde json crates

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate serde;
 #[macro_use] extern crate serde_derive;
+extern crate serde_json;
 #[macro_use] extern crate diesel;
 extern crate r2d2_diesel;
 extern crate r2d2;
